d2a0a7faa361a354d9356051421855ff6ae914ce
Pedro Mendes <107001055+PedroMends30@users.noreply.github.com>
2025-05-19 05:52:59 -0300

    Visor do numero de repeticao 2.0

--- hello.c
+++ hello.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    for (int i = 0; i < 3; i++) {
-        printf("Hello World! [%d]\n", i + 1);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <num_repeticoes>\n", argv[0]);
+        return 1;
     }
+
+    int n = atoi(argv[1]);
+    if (n <= 0) {
+        fprintf(stderr, "Número inválido de repetições.\n");
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        printf("Repetição %d: Hello World!\n", i);
+    }
+
     return 0;
 }
